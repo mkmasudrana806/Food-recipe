222740eecd8c887c39e972999436b7f4249d3a5a
mkmasudrana806 <mkmasudrana806@gmail.com>
2023-06-09 20:27:04 +0600

    recipe client connect to github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h3>Welcome to the Food Recipe Hunts</h3>
+    </div>
+  );
+}
+
+export default App;
